3780d36e78dd453044048aa71262aad2ba500d71
mathzb <mathias.baungaard@gmail.com>
2023-07-03 15:58:09 +0200

    added two new routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Support from "./components/Support.jsx";
 import Home from "./components/Home.jsx";
 import Opstart from "./components/Opstart.jsx";
+import Sekretærservice from "./components/Sekretærservice.jsx";
+import Callcenter from "./components/Callcenter.jsx";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/support" element={<Support />} />
         <Route path="/opstart" element={<Opstart />} />
+        <Route path="/sekretærservice" element={<Sekretærservice />} />
+        <Route path="/callcenter" element={<Callcenter />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
