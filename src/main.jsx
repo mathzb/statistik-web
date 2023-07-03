915602fa746a33dd08bf31711996186b28ee835f
mathzb <mathias.baungaard@gmail.com>
2023-07-03 14:29:05 +0200

    removed space

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-
       <App />
     </ThemeProvider>
   </React.StrictMode>
