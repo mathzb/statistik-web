592174300fd07e23009cb325b6cd24496f780700
Mathias Baungaard Jakobsen <mathias.jacobsen@ipnordic.dk>
2024-09-19 11:58:00 +0200

    fix

--- src/components/Opstart.jsx
+++ src/components/Opstart.jsx
@@ -18,7 +18,7 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Tooltip from "@mui/material/Tooltip";
 import { styled } from "@mui/material/styles";
-import { addDayToDate, calculateCallsTransfersAndPause, handleDisableButton, checkDateDifference } from "../utils";
+import { addDayToDate, calculateCallsTransfersAndPause, checkDateDifference, handleDisableButton } from "../utils";
 import Navbar from "./Navbar";
 import { fetchApiData } from "../api";
 
